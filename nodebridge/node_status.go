2df34d21356f4d371c8ec06b24dd10856a480473
Alexander Sporn <github@alexsporn.de>
2022-09-09 13:59:12 +0200

    Adapt nodebridge to use the new ListenToNodeStatus

---
+++ nodebridge/node_status.go
@@ -0,0 +1,127 @@
+package nodebridge
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	"github.com/iotaledger/hive.go/serializer/v2"
+	inx "github.com/iotaledger/inx/go"
+	iotago "github.com/iotaledger/iota.go/v3"
+)
+
+func (n *NodeBridge) NodeStatus() *inx.NodeStatus {
+	n.nodeStatusMutex.RLock()
+	defer n.nodeStatusMutex.RUnlock()
+
+	return n.nodeStatus
+}
+
+func (n *NodeBridge) IsNodeHealthy() bool {
+	return n.NodeStatus().GetIsHealthy()
+}
+
+func (n *NodeBridge) IsNodeSynced() bool {
+	return n.NodeStatus().GetIsSynced()
+}
+
+func (n *NodeBridge) IsNodeAlmostSynced() bool {
+	return n.NodeStatus().GetIsAlmostSynced()
+}
+
+func (n *NodeBridge) ProtocolParameters() *iotago.ProtocolParameters {
+	n.nodeStatusMutex.RLock()
+	defer n.nodeStatusMutex.RUnlock()
+
+	return n.protocolParameters
+}
+
+func protocolParametersFromRaw(params *inx.RawProtocolParameters) (*iotago.ProtocolParameters, error) {
+	if params.ProtocolVersion != supportedProtocolVersion {
+		return nil, fmt.Errorf("unsupported protocol version %d vs %d", params.ProtocolVersion, supportedProtocolVersion)
+	}
+
+	protoParams := &iotago.ProtocolParameters{}
+	if _, err := protoParams.Deserialize(params.GetParams(), serializer.DeSeriModeNoValidation, nil); err != nil {
+		return nil, err
+	}
+
+	return protoParams, nil
+}
+
+func (n *NodeBridge) listenToNodeStatus(ctx context.Context, cancel context.CancelFunc) error {
+	defer cancel()
+
+	stream, err := n.client.ListenToNodeStatus(ctx, &inx.NodeStatusRequest{CooldownInMilliseconds: 1_000})
+	if err != nil {
+		return err
+	}
+
+	for {
+		nodeStatus, err := stream.Recv()
+		if err != nil {
+			if errors.Is(err, io.EOF) || status.Code(err) == codes.Canceled {
+				break
+			}
+			n.LogErrorf("listenToNodeStatus: %s", err.Error())
+
+			break
+		}
+		if ctx.Err() != nil {
+			break
+		}
+
+		if err := n.processNodeStatus(nodeStatus); err != nil {
+			n.LogErrorf("processNodeStatus: %s", err.Error())
+			break
+		}
+	}
+
+	//nolint:nilerr // false positive
+	return nil
+}
+
+func (n *NodeBridge) processNodeStatus(nodeStatus *inx.NodeStatus) error {
+
+	var latestMilestoneChanged bool
+	var confirmedMilestoneChanged bool
+
+	n.nodeStatusMutex.Lock()
+	if nodeStatus.GetLatestMilestone().GetMilestoneInfo().GetMilestoneIndex() > n.nodeStatus.GetLatestMilestone().GetMilestoneInfo().GetMilestoneIndex() {
+		latestMilestoneChanged = true
+	}
+
+	if nodeStatus.GetConfirmedMilestone().GetMilestoneInfo().GetMilestoneIndex() > n.nodeStatus.GetConfirmedMilestone().GetMilestoneInfo().GetMilestoneIndex() {
+		confirmedMilestoneChanged = true
+	}
+	n.nodeStatus = nodeStatus
+
+	protocolParams, err := protocolParametersFromRaw(nodeStatus.GetCurrentProtocolParameters())
+	if err != nil {
+		n.nodeStatusMutex.Unlock()
+		return err
+	}
+	n.protocolParameters = protocolParams
+
+	n.nodeStatusMutex.Unlock()
+
+	if latestMilestoneChanged {
+		milestone, err := milestoneFromINXMilestone(nodeStatus.GetLatestMilestone())
+		if err == nil {
+			n.Events.LatestMilestoneChanged.Trigger(milestone)
+		}
+	}
+
+	if confirmedMilestoneChanged {
+		milestone, err := milestoneFromINXMilestone(nodeStatus.GetConfirmedMilestone())
+		if err == nil {
+			n.Events.ConfirmedMilestoneChanged.Trigger(milestone)
+		}
+	}
+
+	return nil
+}
