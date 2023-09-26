428f62ce29e16698efc69e2303278c0878087ee2
Piotr Macek <4007944+piotrm50@users.noreply.github.com>
2023-09-26 22:13:49 +0200

    Make slot and epoch variable name consistent

--- pkg/nodebridge/tangle_listener.go
+++ pkg/nodebridge/tangle_listener.go
@@ -147,7 +147,7 @@ func (t *TangleListener) RegisterSlotConfirmedEvent(sIndex uint32) *valuenotifie
 		// this should never fail
 		panic(err)
 	}
-	if comm != nil && comm.Commitment.Index >= iotago.SlotIndex(sIndex) {
+	if comm != nil && comm.Commitment.Index >= iotago.Slot(sIndex) {
 		// trigger the sync event, because the slot is already confirmed
 		t.commitmentConfirmedNotifier.Notify(sIndex)
 	}
