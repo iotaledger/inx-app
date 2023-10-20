22fbb87c3a0ff3a879b3e42e8431657472529af6
Piotr Macek <4007944+piotrm50@users.noreply.github.com>
2023-10-20 12:00:46 +0200

    Add account check endpoints

---
+++ pkg/nodebridge/accounts.go
@@ -0,0 +1,34 @@
+package nodebridge
+
+import (
+	"context"
+
+	inx "github.com/iotaledger/inx/go"
+	iotago "github.com/iotaledger/iota.go/v4"
+)
+
+func (n *NodeBridge) ReadIsCandidate(ctx context.Context, id iotago.AccountID, slot iotago.SlotIndex) (bool, error) {
+	result, err := n.client.ReadIsCandidate(ctx, inx.NewAccountInfoRequest(id, slot))
+	if err != nil {
+		return false, err
+	}
+
+	return result.GetValue(), nil
+}
+
+func (n *NodeBridge) ReadIsCommitteeMember(ctx context.Context, id iotago.AccountID, slot iotago.SlotIndex) (bool, error) {
+	result, err := n.client.ReadIsCommitteeMember(ctx, inx.NewAccountInfoRequest(id, slot))
+	if err != nil {
+		return false, err
+	}
+
+	return result.GetValue(), nil
+}
+func (n *NodeBridge) ReadIsAccountValidator(ctx context.Context, id iotago.AccountID, slot iotago.SlotIndex) (bool, error) {
+	result, err := n.client.ReadIsAccountValidator(ctx, inx.NewAccountInfoRequest(id, slot))
+	if err != nil {
+		return false, err
+	}
+
+	return result.GetValue(), nil
+}
